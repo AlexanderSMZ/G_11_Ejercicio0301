2bd387792b9921343f3007536050d741a8e40a4f
alexa <alexa@DESKTOP-U11IB3O>
2022-05-13 04:35:24 -0500

    V1

---
+++ src/main/java/com/mycompany/g11_ejercicio0301/Flow_Layout.java
@@ -0,0 +1,90 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.g11_ejercicio0301;
+
+import java.awt.Color;
+import java.awt.FlowLayout;
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.BorderFactory;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+
+/**
+ *
+ * @author alexa
+ */
+public class Flow_Layout extends JFrame{
+    
+    private JPanel jPanel1;
+    private List<JLabel> jLabelList;
+    
+   public Flow_Layout(String title) {
+        super(title);
+        this.setSize(400, 75);
+        this.setLocation(200, 250);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.iniciarComponentes();
+        this.setContentPane(this.jPanel1);
+    }
+
+   public void iniciarComponentes(){
+        this.iniciarPanel();
+        this.iniciarEtiquetas();
+   }
+   
+   public void iniciarPanel(){
+       this.jPanel1 = new JPanel();
+       this.jPanel1.setBackground(Color.GRAY);
+       //this.jPanel1.setBorder(BorderFactory.createTitledBorder("Panel Principal"));
+       this.setLayout(new FlowLayout());
+   }
+   
+   public void iniciarEtiquetas(){
+       
+       this.jLabelList = new ArrayList<>();
+       
+       for(var i=0;i<5;i++)
+        this.jLabelList.add(new JLabel());
+       //this.jLabelList.add(new JLabel());
+       
+       for(var a=0;a<5;a++)
+        this.jLabelList.get(a).setBorder(BorderFactory.createTitledBorder(""));       
+       
+       this.jLabelList.get(0).setBackground(Color.GRAY);
+       this.jLabelList.get(0).setOpaque(true);
+       this.jLabelList.get(0).setText("Button 1");
+       this.jPanel1.add(this.jLabelList.get(0));
+       
+       //////////////
+       this.jLabelList.get(1).setBackground(Color.GRAY);
+       this.jLabelList.get(1).setOpaque(true);
+       this.jLabelList.get(1).setText("2");
+       this.jPanel1.add(this.jLabelList.get(1));
+       //////////////
+       this.jLabelList.get(2).setBackground(Color.GRAY);
+       this.jLabelList.get(2).setOpaque(true);
+       this.jLabelList.get(2).setText("Button 3");
+       this.jPanel1.add(this.jLabelList.get(2));
+       //////////////
+       this.jLabelList.get(3).setBackground(Color.GRAY);
+       this.jLabelList.get(3).setOpaque(true);
+       this.jLabelList.get(3).setText("Long_Named Button 4");
+       this.jPanel1.add(this.jLabelList.get(3));
+       //////////////
+       this.jLabelList.get(4).setBackground(Color.GRAY);
+       this.jLabelList.get(4).setOpaque(true);
+       this.jLabelList.get(4).setText("Button 5");
+       this.jPanel1.add(this.jLabelList.get(4));
+   
+       //this.jLabelList.get(1).setHorizontalAlignment(SwingConstants.CENTER);
+       
+       
+       
+   }
+    
+}
---
+++ src/main/java/com/mycompany/g11_ejercicio0301/PrincipalEjercicio0301.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.g11_ejercicio0301;
+
+/**
+ *
+ * @author alexa
+ */
+public class PrincipalEjercicio0301 {
+    
+        public static void main(String[] args) {
+
+        var v1 = new Border_Layout ("Border Layout");
+        v1.setVisible(true); 
+        
+        var v2 = new GrindLayout ("Grind Layout");
+        v2.setVisible(true);         
+        
+        var v3 = new Flow_Layout ("Flow Layout");
+        v3.setVisible(true);
+        
+        var v4 = new bandera("Border Layout Bandera");
+        v4.setVisible(true); 
+        
+    }
+    
+}
